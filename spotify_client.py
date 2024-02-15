1ecb7269217169d2a50617454303c578f922140c
szmisztal <sz.misztal@gmail.com>
2024-02-15 12:20:05 +0100

    add serach_query func

--- spotify_client.py
+++ spotify_client.py
@@ -11,13 +11,19 @@ def get_api_token():
       response = requests.post(url, data = data)
       return response.json().get("access_token")
 
-# def call_spotify_api(endpoint):
-#    full_url = f"https://api.spotify.com/{endpoint}"
-#    headers = {
-#        "Authorization": f"Bearer {api_token}"
-#    }
-#    response = requests.get(full_url, headers = headers)
-#    response.raise_for_status()
-#    return response.json()
+def call_spotify_api(endpoint):
+    api_token = get_api_token()
+    full_url = f"https://api.spotify.com/v1/{endpoint}"
+    headers = {
+       "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(full_url, headers = headers)
+    response.raise_for_status()
+    return response.json()
 
-print(get_api_token())
+def search_query(query):
+    response = call_spotify_api(f"search?q={query}&type=album")
+    return response
+
+
+print(search_query("Demonologia"))
