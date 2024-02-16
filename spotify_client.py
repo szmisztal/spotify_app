8809cf87f00be1c7e053d45dec6be5168536ec71
szmisztal <sz.misztal@gmail.com>
2024-02-16 12:54:09 +0100

    add user_inputs function and import urllib to encode it

--- spotify_client.py
+++ spotify_client.py
@@ -1,3 +1,4 @@
+import urllib
 import requests
 from secrets import client_id, client_secret
 
@@ -21,9 +22,19 @@ def call_spotify_api(endpoint):
     response.raise_for_status()
     return response.json()
 
-def search_query(query):
-    response = call_spotify_api(f"search?q={query}&type=album")
+def search_query():
+    encoded_user_inputs = user_inputs()
+    album = encoded_user_inputs[0]
+    artist = encoded_user_inputs[1]
+    response = call_spotify_api(f"search?q={album}+artist:{artist}&type=album")
     return response
 
+def user_inputs():
+    album_input = input("Album: ")
+    artist_input = input("Artist: ")
+    album_encode = urllib.parse.quote(album_input)
+    artist_encode = urllib.parse.quote(artist_input)
+    return (album_encode, artist_encode)
 
-print(search_query("Demonologia"))
+
+print(search_query())
